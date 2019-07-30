a84c208e12d9e1705ffb80102b9148f4302a03f9
Brian Doan <briandoan71@yahoo.com>
2019-07-30 11:41:04 -0400

    Revert "brian"
    This reverts commit d386c80b23425dbe1764376f806780f76a7d4e7a.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MollyComponent } from './molly/molly.component';
-import { BrianComponent } from './brian/brian.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MollyComponent,
-    BrianComponent
+    MollyComponent
   ],
   imports: [
     BrowserModule,
