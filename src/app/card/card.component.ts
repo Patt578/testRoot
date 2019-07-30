c5d648ba39747acb47c1dc4dd6079c27f24f0ff1
Patrick Bassett <bassettptty@gmail.com>
2019-07-30 16:29:34 -0400

    first cards commit

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -7,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  name:string = '';
+  height:number = 0;
+  mass:number = 0;
+  hair_color:string = '';
+  skin_color:string = '';
+  eye_color:string ='';
+  birth_year:string ='';
+  gender:string ='';
+
+  title:string = '';
+  episode_id:number = 0;
+  opening_crawl:string = '';
+  director:string = '';
+  producer:string ='';
+  release_date:string ='';
+
+  ship_name:string = '';
+  model:string = '';
+  cost_in_credits:number = 0;
+  length:number = 0;
+  max_atmospheric_speed:number = 0;
+  crew:number = 0;
+  passengers:number = 0;
+  cargo_capacity:number = 0;
+  consumables:string = '';
+  hyperdrive_rating:number = 0;
+  mglt:string ='';
+
+
+
+  
   constructor() { }
 
   ngOnInit() {
