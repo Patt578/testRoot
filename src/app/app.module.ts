9fa0040f79cd1367aae59404e9b3fb026198c042
Brian Doan <briandoan71@yahoo.com>
2019-07-30 14:01:13 -0400

    search form wireframe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MollyComponent } from './molly/molly.component';
 import { BrianComponent } from './brian/brian.component';
 import { SwapiSearchComponent } from './swapi-search/swapi-search.component';
+import { FrontendSearchFunctionComponent } from './frontend-search-function/frontend-search-function.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MollyComponent,
     BrianComponent,
-    SwapiSearchComponent
+    SwapiSearchComponent,
+    FrontendSearchFunctionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSelectModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/frontend-search-function/frontend-search-function.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-frontend-search-function',
+  templateUrl: './frontend-search-function.component.html',
+  styleUrls: ['./frontend-search-function.component.css']
+})
+export class FrontendSearchFunctionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
