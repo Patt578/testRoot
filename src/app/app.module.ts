9885c4675710b959955ffc9796f79cb0533e9941
Molly Hopper <mollyhopper95@gmail.com>
2019-07-30 14:28:14 -0400

    got fetch to work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MollyComponent } from './molly/molly.component';
 import { BrianComponent } from './brian/brian.component';
 import { SwapiSearchComponent } from './swapi-search/swapi-search.component';
+import { FetchSwapiService } from './fetch-swapi.service'
+// import { FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -16,9 +19,10 @@ import { SwapiSearchComponent } from './swapi-search/swapi-search.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [FetchSwapiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
