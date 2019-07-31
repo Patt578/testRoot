ee3d305567f4e5937482dd69fd639379211cc16a
Brian Doan <briandoan71@yahoo.com>
2019-07-31 13:40:08 -0400

    ngIf

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -93,6 +93,8 @@ export class CardComponent implements OnChanges {
     console.log(`grabbed ${this.data.name}`)
 
   }
+
+  show: boolean = false;
   
   ngOnChanges() {
     if(this.data['title'] !== undefined){
@@ -107,7 +109,7 @@ export class CardComponent implements OnChanges {
   }    
 
   // ngOnInit() {
-  //   // this.grabData()
+  //   this.ngOnChanges()
   // }
 
 }
